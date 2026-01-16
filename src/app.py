272715edbe3bf9499432e00f5a65d1eed9ac8680
jadonscombs <104600260+jadonscombs@users.noreply.github.com>
2026-01-16 01:15:54 -0800

    Add Flask server component

--- src/app.py
+++ src/app.py
@@ -3,3 +3,29 @@ Project OneLight.
 
 Application entry point.
 """
+
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    # TODO: Return a basic HTML page with two clickable buttons:
+    # - button 1: "ON"
+    # - button 2: "OFF"
+    return 'Index page. Nothing to see here...'
+
+@app.route("/on")
+def turn_on():
+    return "Light turned on!"
+
+@app.route("/off")
+def turn_off():
+    return "Light turned off!"
+
+
+# Development -- make safer adjustments later
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=3000, debug=True)
