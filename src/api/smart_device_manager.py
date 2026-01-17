a012540cef094167c0cc290fad28181afd8f5e04
jadonscombs <104600260+jadonscombs@users.noreply.github.com>
2026-01-16 16:42:39 -0800

    Verified turn on/off functionality over web server

---
+++ src/api/smart_device_manager.py
@@ -0,0 +1,166 @@
+"""
+Project OneLight.
+
+Scaffolding and implementation for interacting with TP-Link HS100 Smart Plug.
+
+Follow these guides (in preferred order):
+1. https://github.com/python-kasa/python-kasa?tab=readme-ov-file (larger support)
+2. https://github.com/jkbenaim/hs100 (Linux-only)
+
+Provisioning a new smart plug device:
+https://python-kasa.readthedocs.io/en/latest/cli.html#provisioning
+
+$ kasa --host 192.168.0.1 wifi join "YOUR_SSID"
+
+When prompted for "Keytype," enter "3" (no quotes) for WPA2-PSK
+
+
+Once provisioned, you can discover HS100 using the command:
+$ kasa --target XXX.XXX.XXX.255 --type plug discover
+
+    (just replace "XXX.XXX.XXX" with the network portion that your development
+    device is on. often it is "192.168.0" or "192.168.1")
+"""
+
+import asyncio
+from kasa import Device, DeviceConfig, Discover
+import subprocess
+import json
+import logging
+import pathlib
+import pprint
+import traceback
+from typing import Optional
+
+logger = logging.getLogger("onelight-app")
+
+DEFAULT_CONFIG_PATH = pathlib.Path(__file__).parents[2] / ".ignore" / "config.json"
+DEFAULT_BROADCAST = "255.255.255.255"
+NULL_IP = "X.X.X.X"
+HS100 = "hs100"
+UNKNOWN = "UNKNOWN"
+
+CODE_200 = "200"
+CODE_400 = "400"
+
+# Lightweight device cache
+# - a device type (e.g., "hs100") can have 2+ devices listed by {ip:config}
+device_cache = {
+    HS100: {
+    }
+}
+
+
+async def get_hs100_device() -> Optional[Device]:
+    """
+    Returns only the first HS100 device accessible.
+
+    For now, this is not scalable (2+ HS100s) and will need expansion later.
+    """
+
+    # Check device cache
+    if len(device_cache[HS100]) != 0:
+        device = await Device.connect(host=list(device_cache[HS100].keys())[0])
+        if isinstance(device, Device):
+            return device
+
+    # Check for device via broadcast
+    ip_broadcast_target = get_hs100_broadcast_ip()
+    logger.info(f"Fallback: Search via broadcast IP {ip_broadcast_target}")
+
+    hs100_device_candidates = await Discover.discover(
+        target=ip_broadcast_target,
+        port=9999
+    )
+    if len(hs100_device_candidates) > 0:
+        logger.info(f"Found {len(hs100_device_candidates)} device(s):")
+        for ip, device in hs100_device_candidates.items():
+            conf_ = pprint.pformat(
+                device.config.to_dict(),
+                indent=4,
+                sort_dicts=False
+            )
+            logger.info(f"config:\n{conf_}")
+            if is_hs100_device(device):
+                update_device_cache(device, is_hs100=True)
+                return device
+
+    # Check for device via hard-set endpoint IP
+    try_host_ip = get_hs100_uncertain_ip()
+    logger.info(f"Fallback: Using host IP {try_host_ip}")
+    if try_host_ip is not None:
+        device = await Device.connect(host=try_host_ip)
+        if is_hs100_device(device):
+            conf_ = pprint.pformat(
+                device.config.to_dict(),
+                indent=4,
+                sort_dicts=False
+            )
+            logger.info(
+                f"config:\n{conf_}"
+            )
+            update_device_cache(device, is_hs100=True)
+            return device
+
+    return None
+
+
+async def ping_hs100_device() -> str:
+    """
+    Return 200 if HS100 device reachable.
+
+    Otherwise, return 400 (optionally with reason).
+    """
+    device: Optional[Device] = await get_hs100_device()
+    return CODE_200 if device is not None else CODE_400
+
+
+async def turn_on_hs100(device: Optional[Device] = None):
+    if device is None:
+        device = await get_hs100_device()
+    if isinstance(device, Device):
+        await device.turn_on()
+
+
+async def turn_off_hs100(device: Optional[Device] = None):
+    if device is None:
+        device = await get_hs100_device()
+    if isinstance(device, Device):
+        await device.turn_off()
+
+
+def load_config(json_config_path: pathlib.Path = DEFAULT_CONFIG_PATH):
+    path = pathlib.Path(json_config_path)
+    with open(path, 'r') as config:
+        try:
+            return json.load(config)
+        except Exception:
+            logger.exception("Exception in load_config()...")
+    logger.warning("Returning empty JSON config...")
+    return {}
+
+
+def get_hs100_broadcast_ip():
+    return load_config().get("hs100", {}).get("network", {}).get("broadcast", DEFAULT_BROADCAST)
+
+
+def get_hs100_uncertain_ip():
+    return load_config().get("hs100", {}).get("network", {}).get("uncertain_host_ip", None)
+
+
+def get_hs100_mac():
+    return load_config().get("hs100", {}).get("mac", "XX:XX:XX:XX:XX:XX")
+
+
+def is_hs100_device(device: Device):
+    """
+    Return True if <Device> is confirmed to be an HS100.
+
+    Warning: Currently hard-set on using MAC address to determine this.
+    """
+    return device.device_id == get_hs100_mac()
+
+
+def update_device_cache(device: Device, is_hs100: bool = False):
+    model = HS100 if is_hs100 else UNKNOWN
+    device_cache[model][device.host] = device.config.to_dict()
--- src/app.py
+++ src/app.py
@@ -4,28 +4,66 @@ Project OneLight.
 Application entry point.
 """
 
-from flask import Flask
+import asyncio
+import logging
+from logging.handlers import RotatingFileHandler
+
+from quart import Quart
 from markupsafe import escape
 
+from api.smart_device_manager import (
+    ping_hs100_device,
+    turn_on_hs100,
+    turn_off_hs100
+)
 
-app = Flask(__name__)
+
+KB = 1024
+
+logger = logging.getLogger("onelight-app")
+logger.setLevel(logging.DEBUG)
+
+log_fmt = '%(asctime)s - %(name)s - [%(levelname)s] - %(message)s'
+log_handler = RotatingFileHandler(
+    "onelight-app.log",
+    maxBytes=80*KB,
+    backupCount=4
+)
+log_handler.setLevel(logging.DEBUG)
+log_handler.setFormatter(logging.Formatter(log_fmt))
+
+# Console logging
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(logging.Formatter(log_fmt))
+
+# Add log handlers
+logger.addHandler(log_handler)
+logger.addHandler(console_handler)
+
+app = Quart(__name__)
 
 @app.route("/")
-def index():
+async def index():
     # TODO: Return a basic HTML page with two clickable buttons:
     # - button 1: "ON"
     # - button 2: "OFF"
     return 'Index page. Nothing to see here...'
 
 @app.route("/on")
-def turn_on():
+async def turn_on():
+    await turn_on_hs100()
     return "Light turned on!"
 
 @app.route("/off")
-def turn_off():
+async def turn_off():
+    await turn_off_hs100()
     return "Light turned off!"
 
+@app.route("/hs100_status")
+async def hs100_status():
+    return await ping_hs100_device()
 
 # Development -- make safer adjustments later
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=3000, debug=True)
+    app.run(host='127.0.0.1', port=3000, debug=False)
