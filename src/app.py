ed826dc924f433b58c4399da1e01d4d41c3b2cb3
jadonscombs <104600260+jadonscombs@users.noreply.github.com>
2026-02-05 23:36:51 -0800

    Fix

--- src/app.py
+++ src/app.py
@@ -369,43 +369,6 @@ async def hs100_state():
 
 # Development -- make safer adjustments later
 if __name__ == "__main__":
-    env = None
-
-    # Ensure env param is supplied
-    if len(sys.argv) == 1:
-
-        # Try loading env from secrets.ini
-        try:
-            env = get_app_env()
-        except Exception:
-            logger.fatal(
-                "OneLight cannot start without a specified environment! "
-                "Must be 'dev' or 'prod' and you must have configured your "
-                "'secrets.ini' file."
-            )
-            raise RuntimeError(
-                "OneLight cannot start without a specified environment! "
-                "Must be 'dev' or 'prod' and you must have configured your "
-                "'secrets.ini' file."
-            )
-
-    # Detect env
-    else:
-        env = sys.argv[1]
-        if "env=" in env:
-            env = env[4:].strip()
-            if env.startswith("dev"):
-                env = "dev"
-            elif env.startswith("prod"):
-                env = "prod"
-            else:
-                logger.debug(f"Invalid env CLI param '{env}'")
-                env = "dev"
-        else:
-            logger.debug(f"Invalid env CLI param '{env}'")
-            env = "dev"
-
-    logger.debug(f"Using application env '{env}'")
 
     # Get proper host
     host = DevConfig.HOST if env == "dev" else ProdConfig.HOST
